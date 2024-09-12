6227b020aa25154d8960caceb65bac78ca642f01
Lam Vo <votruonglam2109@gmail.com>
2024-09-12 23:32:41 +0700

    init

---
+++ BLL/AccountService.cs
@@ -0,0 +1,38 @@
+﻿using CoffeeManagement.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeManagement.BLL
+{
+    public class AccountService
+    {
+
+        #region Singleton pattern
+        private static AccountService instance;
+        public static AccountService Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new AccountService();
+                return instance;
+            }
+            private set { instance = value; }
+        }
+        #endregion
+
+        public bool isLoginSucess(string username, string password)
+        {
+            return AccountDAL.Instance.CountByUsernameAndPassword(username, password) > 0;
+        }
+
+        public DataRow GetAccountByUsername(string username)
+        {
+            return AccountDAL.Instance.GetAccountByUsername(username);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using CoffeeManagement.pdf;
+using CoffeeManagement.qrCode;
+using System;
+using System.Windows.Forms;
+
+using CoffeeManagement.management;
+
+namespace CoffeeManagement
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+            //new PDFGenerator("invoice.xml").CreateDocument();
+        }
+    }
+}
